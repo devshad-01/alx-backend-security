eb480781e7061d3c888b8e8bcab144bded0b4f5b
Student <student@alx.com>
2025-07-26 12:46:49 +0300

    initial commit

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,10 +1,13 @@
 from django.utils import timezone
 from django.http import HttpResponseForbidden
+from django.core.cache import cache
+from ipgeolocation import IpGeolocationAPI
 from .models import RequestLog, BlockedIP
 
 class IPTrackingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.geo_api = IpGeolocationAPI()
 
     def __call__(self, request):
         ip_address = self.get_client_ip(request)
@@ -12,10 +15,20 @@ class IPTrackingMiddleware:
         # Block request if IP is blacklisted
         if BlockedIP.objects.filter(ip_address=ip_address).exists():
             return HttpResponseForbidden('Forbidden: Your IP is blocked.')
+        # Geolocation caching (24 hours)
+        geo_cache_key = f'geo_{ip_address}'
+        geo_data = cache.get(geo_cache_key)
+        if not geo_data:
+            geo_data = self.geo_api.get_geolocation(ip_address)
+            cache.set(geo_cache_key, geo_data, 60 * 60 * 24)  # 24 hours
+        country = geo_data.get('country_name', '') if geo_data else ''
+        city = geo_data.get('city', '') if geo_data else ''
         RequestLog.objects.create(
             ip_address=ip_address,
             timestamp=timezone.now(),
-            path=path
+            path=path,
+            country=country,
+            city=city
         )
         response = self.get_response(request)
         return response
--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -4,9 +4,11 @@ class RequestLog(models.Model):
     ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
+    country = models.CharField(max_length=64, blank=True, null=True)
+    city = models.CharField(max_length=64, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.ip_address} @ {self.timestamp} -> {self.path}"
+        return f"{self.ip_address} @ {self.timestamp} -> {self.path} ({self.country}, {self.city})"
 
 
 class BlockedIP(models.Model):
